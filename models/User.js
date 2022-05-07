30283bb15eec566772b842a2910138cbd80de119
Colby Harris <harris.colby@gmail.com>
2022-05-07 04:51:18 -0500

    added models

--- models/User.js
+++ models/User.js
@@ -1,9 +1,44 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
-  
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'Please choose a Username',
+      trim: true
+    },
+    email: {
+      type: String,
+      required: 'Please provide an email',
+      match: /.+\@.+\..+/,
+      unique: true
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],    
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+);
+
+UserSchema.virtual('friendCount').get(()=>{
+  return this.friends.length;
 });
 
-const Pizza = model('Pizza', UserSchema);
+const User = model('User', UserSchema);
 
 module.exports = User;
